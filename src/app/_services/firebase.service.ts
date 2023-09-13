bbd24a8f4013855aed6008574b05c9545f2c5d58
hadeer bayoumi <hadeer.bayumi.ext@orange.com>
2023-09-13 14:36:35 +0300

    add firebase app configs

--- src/app/_services/firebase.service.ts
+++ src/app/_services/firebase.service.ts
@@ -11,8 +11,14 @@ export class FirebaseService {
 
   // TODO: Replace the following with your app's Firebase project configuration
   readonly firebaseConfig = {
-    //...
-  };
+    apiKey: "AIzaSyDM09nMorzgVrmPIZIvd6TZkqitcDdz4Bw",
+    authDomain: "crises-response-b46d0.firebaseapp.com",
+    projectId: "crises-response-b46d0",
+    storageBucket: "crises-response-b46d0.appspot.com",
+    messagingSenderId: "984067287775",
+    appId: "1:984067287775:web:e71f1ef4c8293cdcbc576a",
+    measurementId: "G-XQLM5WZJWK"
+    };
 
   readonly firebaseApp: any;
   readonly fireAuth: any;
