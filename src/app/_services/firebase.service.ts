074be4dbf71d4e32bc33802b05bd12d52d8b4f2c
marinaBergas <Mohamed.khaled+1-at-787715290512>
2023-09-13 16:13:04 +0300

    feature: update firebaseService

--- src/app/_services/firebase.service.ts
+++ src/app/_services/firebase.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@angular/core';
 import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
+import { getAuth, signInWithPhoneNumber, RecaptchaVerifier } from "firebase/auth";
 
 @Injectable({
   providedIn: 'root'
 })
 export class FirebaseService {
-
+  appVerifier: any;
+  confirmationResult: any;
 
   // TODO: Replace the following with your app's Firebase project configuration
   readonly firebaseConfig = {
@@ -18,7 +19,7 @@ export class FirebaseService {
     messagingSenderId: "984067287775",
     appId: "1:984067287775:web:e71f1ef4c8293cdcbc576a",
     measurementId: "G-XQLM5WZJWK"
-    };
+  };
 
   readonly firebaseApp: any;
   readonly fireAuth: any;
@@ -30,7 +31,52 @@ export class FirebaseService {
   }
 
   authinticateUser() {
+    const phoneNumber = "+201272747752";
+    if (!this.appVerifier) this.recaptcha();
+    signInWithPhoneNumber(this.fireAuth, phoneNumber, this.appVerifier)
+      .then((confirmationResult: any) => {
+        this.confirmationResult = confirmationResult;
+        console.log(confirmationResult);
+        return confirmationResult;
+        // SMS sent. Prompt user to type the code from the message, then sign the
+        // user in with confirmationResult.confirm(code).
+        // window.confirmationResult = confirmationResult;
+        // ...
+      }).catch((error) => {
+        // Error; SMS not sent
+        // ...
+      });
+  }
 
+  recaptcha() {
+    this.appVerifier = new RecaptchaVerifier(this.fireAuth, 'sign-in-button', {
+      size: 'invisible',
+      callback: ((response: any) => {
+        console.log('capresponse', response);
+      }),
+      'expired-callback': () => { },
+      'error-callback': ((e: any) => {
+        console.log("Error occurred", e);
+      })
+    });
+  }
+
+  async verifyOtp(otp: string, password?: string) {
+    return new Promise(async (resolve, reject) => {
+      try {
+        if (!this.appVerifier) {
+          this.recaptcha();
+        }
+        const result = this.confirmationResult.confirm(otp).then((result: any) => {
+          // User signed in successfully.
+          const user = result.user;
+          // ...
+        });
+      } catch (e) {
+        // console.log("Verfy Err : ", e?.message);
+        reject(e);
+      }
+    });
   }
 
 
